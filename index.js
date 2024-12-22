9dbb55d913c1323a320d30fc902a272ed261e225
Santosh Managuli <santoshmanaguli98@gmail.com>
2024-12-22 19:17:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function calculateSum(first, second) {
+    return first + second;
+}
+var result = calculateSum(1, 2);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function calculateSum(first: number, second: number): number{
+  return first + second
+}
+
+const result = calculateSum(1, 2);
+console.log(result)
